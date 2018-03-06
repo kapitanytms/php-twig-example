3975668bcf3bcbff23285246114f6eb857f60197
kapitanytms <kapitanytms@gmail.com>
2018-03-06 16:40:31 +0100

    Little change

--- index.php
+++ index.php
@@ -1,12 +1,45 @@
 <?php
 
 require_once('vendor/autoload.php');
-
+// loads templates from the file system
 $loader = new Twig_Loader_Filesystem('templates');
+
+//$loader = new Twig_Loader_Array(array(
+//    'hey' => 'Welcome {{ name }}'
+//));
+
+//the central object for storing configuration
 $twig = new Twig_Environment($loader, array(
-    'cache' => 'cache'
+    'cache' => 'cache',
+    'debug' => true,
+//    'charset' => 'utf-8'
 ));
 
-$template = $twig->load('index.html');
-echo $template->render(array('name' => 'Tamas'));
+$template = $twig->load('index.twig');
+
+class User {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+}
+
+$users = array(
+    new User('Tamas', 12),
+    new User('John', 12)
+);
+echo $template->render(
+    array(
+        'title' => 'Twig',
+        'users' => $users,
+        'num' => -5
+
+    )
+);
+//these to are equals
+//echo $twig->render('index.twig', array('name' => 'Tamas'));
 
